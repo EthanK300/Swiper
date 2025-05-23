b75362166ce189a2e47b7848062fb82b6208a982
Ethan K <1bluejellybean@gmail.com>
2025-05-23 15:14:05 -0700

    start login functionality

--- app/src/main/java/com/application/swiper/LoginActivity.java
+++ app/src/main/java/com/application/swiper/LoginActivity.java
@@ -2,6 +2,7 @@ package com.application.swiper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 //        startActivity(new Intent(this, MainActivity.class));
+        if(savedInstanceState != null){
+
+        }
+        Button loginButton = (Button)this.findViewById(R.id.login);
+        Button createButton = (Button)this.findViewById(R.id.create);
+        Button continueButton = (Button)this.findViewById(R.id.signedout);
+
     }
 }
