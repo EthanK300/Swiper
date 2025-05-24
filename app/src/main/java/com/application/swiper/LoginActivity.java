a5ddddbe654b70014f601ff4897f1f0e1e006082
Ethan <1bluejellybean@gmail.com>
2025-05-23 21:05:56 -0700

    new logo, start handling button callbacks

--- app/src/main/java/com/application/swiper/LoginActivity.java
+++ app/src/main/java/com/application/swiper/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.application.swiper;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +17,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 //        startActivity(new Intent(this, MainActivity.class));
+        SharedPreferences sharedPrefs = getPreferences(Context.MODE_PRIVATE);
         if(savedInstanceState != null){
-
+            System.out.println("exists save");
+        }else{
+            System.out.println("no save exists");
         }
         Button loginButton = (Button)this.findViewById(R.id.login);
         Button createButton = (Button)this.findViewById(R.id.create);
@@ -22,6 +29,6 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(view -> System.out.println("login clicked"));
         createButton.setOnClickListener(view -> System.out.println("create clicked"));
         continueButton.setOnClickListener(view -> System.out.println("continue clicked"));
-        
+
     }
 }
