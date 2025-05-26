2222179a9f31b0efdb80150cfcbf3edf78198fe7
Ethan <1bluejellybean@gmail.com>
2025-05-26 10:33:23 -0700

    add buttons, layout changes

--- app/src/main/java/com/application/swiper/MainActivity.java
+++ app/src/main/java/com/application/swiper/MainActivity.java
@@ -2,18 +2,25 @@ package com.application.swiper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Intent intent;
+    ImageButton settingsButton;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         intent = getIntent();
         setContentView(R.layout.main_activity);
-        System.out.println(intent.getStringExtra("type"));
-//        TextView temp = this.findViewById(R.id.temp);
-
+        System.out.println("session type: " + intent.getStringExtra("type"));
+        settingsButton = this.findViewById(R.id.settingsgear);
+        settingsButton.setOnClickListener(v -> {
+            System.out.println("settings clicked");
+        });
     }
 }
